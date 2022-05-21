8b90ce587466fd89771b64772491942b3ebb8543
sakuexe <saku.karttunen@student.hamk.fi>
2022-05-21 10:45:12 +0300

    added bugfixes and comments

--- downloadvideo.py
+++ downloadvideo.py
@@ -5,12 +5,21 @@ from terminaleffects import tcolors
 
 class videoMerge:
     def combineVideo(video: str, audio: str, destination: str):
+        '''
+            The function takes two strings which include the file names to the audio and video files. Alongside this the desktop path will be given
+            -
+            Afterwards the downloaded files get merged into a single mp4 file with both audio and video.
+            The newly merged video gets saved on the user's desktop.
+        '''
+
         tcolors.clear
         videoStream = ffmpeg.input(f"./temp/{video}")
         audioStream = ffmpeg.input(f"./temp/{audio}")
-        videoTitle = video.replace("video-", "")
+        videoTitle = video.replace("video-", "")    # The filename for the end product gets called here
 
         ffmpeg.concat(videoStream, audioStream, v=1, a=1).output(f"{destination}/{videoTitle}").global_args("-loglevel", "quiet").run()
+        # .global_args("-loglevel", "quiet") make it so that FFMPEG-python does not spam the terminal window with information during the process
+        # this is the program's longest process and can take a lot longer than the initial files' downloads
 
-        for file in os.listdir("./temp/"):
+        for file in os.listdir("./temp/"):              # lastly the files in the temporary directory get removed.
             os.remove(os.path.join("./temp/", file))
--- newyoutubedownloader.py
+++ newyoutubedownloader.py
@@ -1,54 +1,84 @@
 
+from distutils.log import error
+from multiprocessing import reduction
 import pytube
 from pytube import YouTube                          # these two are the main pieces for getting youtube data
 import os
 import time                                         # time module, allows conversion of "seconds" to "MM:SS"
-from terminaleffects import clearTerminal, tcolors  # has terminal colors and the clear function
-from hurry.filesize import size, si                 # helps change bytes to a more readable format
+from terminaleffects import clearTerminal, tcolors  # object with terminal colors and the clear function
+from hurry.filesize import size, si                 # helps converting bytes to a more readable format
 from downloadvideo import videoMerge                # merges video and audio together
 
 
 
 def printInfo(userLink: YouTube):
+    '''
+        Function that prints out information about the video, to make sure the link was working properly
+    '''
     clearTerminal.CLEAR()
 
-    # Ylimääräisiä tietoja videosta joka auttaa varmistamaan että oikea video löytyi
     print(f"Title:{tcolors.bold} {userLink.title} {tcolors.clear}")
     print(f"Views:{tcolors.bold} {userLink.views} {tcolors.clear}")
-    print(f"Length:{tcolors.bold} {time.strftime('%M:%S', time.gmtime(userLink.length))} {tcolors.clear}")
+    print(f"Length:{tcolors.bold} {time.strftime('%M:%S', time.gmtime(userLink.length))} {tcolors.clear}") # Prints out the time in a MM:SS format, rather than seconds
     #print("Ratings: ", userLink.rating)
     #print("Description: ", yt.description)
 
 
 def chooseQuality(userLink: YouTube):
+    '''
+        Function that prints out the available streams for the given video. and return a user input
+    '''
+
     print("\r\nAvailable versions: \r\n")
 
+    streamItags = []    # makes a list to put all the itags to check later
+
     print("---Video---")
-    for stream in userLink.streams.filter(file_extension="mp4", only_video=True):
+    for stream in userLink.streams.filter(file_extension="mp4", only_video=True):   # Prints out all available video streams in the mp4 format
         print(f"{tcolors.cyan}{stream.itag}. {tcolors.clear} | {stream.resolution} | {stream.fps}fps | {size(stream.filesize, system=si)}")
+        streamItags.append(stream.itag)
 
     print("---Audio---")
-    for stream in userLink.streams.filter(only_audio=True):
+    for stream in userLink.streams.filter(only_audio=True):     # Prints out all available audio streams
         print(f"{tcolors.cyan}{stream.itag}. {tcolors.clear} | {stream.abr} | {size(stream.filesize, system=si)} | {stream.mime_type}")
+        streamItags.append(stream.itag)
+    
+    streamChoice = input(f"{tcolors.bold}Choose the version to be downloaded {tcolors.clear} \r\n(from the cyan indexes): {tcolors.cyan}")
 
-    return input(f"{tcolors.bold}Choose the version to be downloaded {tcolors.clear} \r\n(from the cyan indexes): {tcolors.cyan}")
+    if int(streamChoice) in streamItags:
+        return streamChoice
+    else:
+        raise Exception(f"{tcolors.red}The given itag does not match any streams. {tcolors.clear}")
+
+    # returns the user's input, which is expected to be the desired stream's itag number.
+    # if the input isnt found on the list of itags, throws an error.
 
 
 def downloadVideo(userLink: YouTube, quality: str):
-    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-    video = userLink.streams.get_by_itag(quality)
+    '''
+        function that takes the given video and desired format and downloads it to the desktop
+    '''
+
+    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')      # Find's the current PC's path to desktop directory
+    video = userLink.streams.get_by_itag(quality)                                   # and saves the desired stream by the given itag number
 
     if "video" in video.mime_type:
+        # if the stream is a video, it gets downloaded into a temporary directory
+        # after that the highest quality audio stream gets downloaded into the same directory
+
         video.download("./temp/", filename=f"video-{video.default_filename}")
         userLink.streams.get_audio_only().download("./temp/", filename=f"audio-{video.default_filename}")
 
+        # after the video and audio files have been downloaded, combineVideo function will be given the downloaded files names and the desktop path
         videoMerge.combineVideo(f"video-{video.default_filename}", f"audio-{video.default_filename}", desktop)
     else:
+        # if the chosen stream is audio only, the file will get downloaded to the user's desktop folder
         video.download(desktop)
     
     clearTerminal.CLEAR()
 
     print(f"{tcolors.green}Video was downloaded to {tcolors.bold}{desktop}. {tcolors.clear}")
+    # Lastly the confirmation that the process was completed and the path to the user's desktop directory get printed out
 
 
 def main():
@@ -56,9 +86,15 @@ def main():
     print("Youtube Downloader - By Saku")
     print(f"- {pytube.__version__}")
     userLink = input("Insert the link of the video: ")
-    userLink = YouTube(userLink)
 
-    printInfo(userLink)
+    try:
+        userLink = YouTube(userLink)
+        printInfo(userLink)
+    except:
+        print(f"{tcolors.red}Couldn't find a Youtube video with the given link {tcolors.clear}")
+        input("Press enter to exit")
+        return
+
     try:
         quality = chooseQuality(userLink)
     except Exception as e:
